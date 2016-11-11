a775141a035d0e47a4fec13ec5eed0df34d6b70c
Olivier Despesse <olived19@gmail.com>
2016-11-11 23:35:49 +0100

    Install unit tests infrastructure (mocha + chai)

--- install_dev_env.sh
+++ install_dev_env.sh
@@ -10,5 +10,12 @@ docker run -it --rm -v "$PROJECT_DIRECTORY":/usr/src/app -w /usr/src/app node:6.
 printf '** Lib **\n'
 printf 'Installing Babel...\n'
 docker run -it --rm -v "$PROJECT_DIRECTORY":/usr/src/app -w /usr/src/app node:6.2 npm install --save-dev babel-core babel-loader babel-preset-es2015 babel-preset-react
+printf 'Installing Immutable...\n'
+docker run -it --rm -v "$PROJECT_DIRECTORY":/usr/src/app -w /usr/src/app node:6.2 npm install --save immutable
+printf '** Tests **\n'
+printf 'Installing Mocha and Chai...\n'
+docker run -it --rm -v "$PROJECT_DIRECTORY":/usr/src/app -w /usr/src/app node:6.2 npm install --save-dev mocha chai chai-immutable
+printf 'Installing Jsdom...\n'
+docker run -it --rm -v "$PROJECT_DIRECTORY":/usr/src/app -w /usr/src/app node:6.2 npm install --save-dev jsdom
 
 sudo chown -R $USER:$USER "$PROJECT_DIRECTORY"/{node_modules,package.json,webpack.config.js}
