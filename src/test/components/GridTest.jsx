69b817a17cb4a52403c40535c376614e349bf5a8
Olivier Despesse <olived19@gmail.com>
2016-11-18 19:40:09 +0100

    Add a unit test on the grid component to display a complex grid

--- src/test/components/GridTest.jsx
+++ src/test/components/GridTest.jsx
@@ -23,4 +23,24 @@ describe('Grid', () => {
         expect(tds[0].textContent).to.equal('0');
     });
 
+    it('Render a complex world', () => {
+        const WORLD=[
+            [0, 0, 1],
+            [1, 1, 0],
+        ];
+        const component = renderIntoDocument(
+            <Grid world={WORLD} />
+        );
+
+        const trs = scryRenderedDOMComponentsWithTag(component, 'tr');
+        expect(trs.length).to.equal(2);
+
+        const tds = scryRenderedDOMComponentsWithTag(component, 'td');
+        expect(tds[0].textContent).to.equal('0');
+        expect(tds[1].textContent).to.equal('0');
+        expect(tds[2].textContent).to.equal('1');
+        expect(tds[3].textContent).to.equal('1');
+        expect(tds[4].textContent).to.equal('1');
+        expect(tds[5].textContent).to.equal('0');
+    });
 });
