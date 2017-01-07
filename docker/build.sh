cc94f2612942c0d0e6a3d8b36bf5019cb94b7aba
Olivier Despesse <olived19@gmail.com>
2017-01-07 18:11:02 +0100

    Dockerize the web GUI server

---
+++ docker/build.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+VERSION=$1
+
+docker run \
+    --rm \
+    -v "$PWD/..":/usr/src/app \
+    -w /usr/src/app \
+    node:6.2 \
+    node_modules/webpack/bin/webpack.js
+
+cp -pr "../dist" .
+sed -i "s/<VERSION>/${VERSION}/g" Dockerfile
+docker build -t "mgol-webgui:${VERSION}" .
+sed -i "s/${VERSION}/<VERSION>/g" Dockerfile
+rm -R "dist"
