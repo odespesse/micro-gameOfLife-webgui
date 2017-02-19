4d30fa664ff55ef1f31fa5bee176fb87792f5a6a
Olivier Despesse <olived19@gmail.com>
2017-02-19 15:03:03 +0100

    Optimize production build with Webpack options

--- docker/build.sh
+++ docker/build.sh
@@ -7,7 +7,7 @@ docker run \
     -v "$PWD/..":/usr/src/app \
     -w /usr/src/app \
     node:6.2 \
-    node_modules/webpack/bin/webpack.js
+    node_modules/webpack/bin/webpack.js --optimize-minimize --define process.env.NODE_ENV="'production'"
 
 cp -pr "../dist" .
 sed -i "s/<VERSION>/${VERSION}/g" Dockerfile
